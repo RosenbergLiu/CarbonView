4c3c5f56ad8d9fff3e90b4d6e3ccc8b0cf7abcca
RoshanLiu <xygenio@hotmail.com>
2023-08-03 23:20:35 +1000

    Update MauiProgram.cs
    use communityToolkit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,36 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
 using GreenITBlazor.Services;
 using Radzen;
+using CommunityToolkit.Maui;
 
 namespace GreenITBlazor;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
-
-		builder.Services.AddMauiBlazorWebView();
-
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        }).UseMauiCommunityToolkit();
+        builder.Services.AddMauiBlazorWebView();
         builder.Services.AddScoped<DialogService>();
         builder.Services.AddScoped<NotificationService>();
         builder.Services.AddScoped<TooltipService>();
         builder.Services.AddScoped<ContextMenuService>();
-
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
         builder.Services.AddSingleton<BillsDBService>();
         builder.Services.AddSingleton<ApiService>();
-
         return builder.Build();
-	}
+    }
 }
