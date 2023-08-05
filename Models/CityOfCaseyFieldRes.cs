ac88181df5ec48c6121ef3d7c315e7379d961fe2
RosenbergLiu <xygenio@hotmail.com>
2023-08-05 19:42:33 +1000

    add gas visualization

--- Models/CityOfCaseyFieldRes.cs
+++ Models/CityOfCaseyFieldRes.cs
@@ -4,8 +4,25 @@ namespace GreenITBlazor.Models
 {
     public class CityOfCaseyFieldRes
     {
+        [JsonIgnore]
+        private string _yearStr;
+
         [JsonProperty("year")]
-        public string Year { get; set; }
+        public string YearStr
+        {
+            get
+            {
+                return _yearStr;
+            }
+            set
+            {
+                _yearStr = value;
+                if (Int32.TryParse(_yearStr, out int year))
+                {
+                    Year = year;
+                }
+            }
+        }
 
         [JsonProperty("emission_source")]
         public string Type { get; set; }
@@ -13,7 +30,26 @@ namespace GreenITBlazor.Models
         [JsonProperty("average_intensity_kwh_per_customer_per_annum")]
         public double? AveKwhPcPa { get; set; }
 
+        [JsonIgnore]
+        private double _AveGjPcPa;
+
         [JsonProperty("average_intensity_gj_per_customer_per_annum")]
-        public double? AveGjPcPa { get; set; }
+        public double? AveGjPcPa {
+            get
+            {
+                return _AveGjPcPa;
+            }
+            set
+            {
+                _AveGjPcPa = (double)value;
+                AveMjPcPa = _AveGjPcPa * 1000;
+            }
+        }
+
+        [JsonIgnore]
+        public double? AveMjPcPa { get; set; }
+
+        [JsonIgnore]
+        public int Year { get; private set; }
     }
 }
